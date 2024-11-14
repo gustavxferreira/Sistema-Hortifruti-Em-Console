f0f14179119c170e622bdf1251ff88923413cb90
gustavxferreira <gustavobusiness170@gmail.com>
2024-11-14 12:51:39 -0300

    Informar categorias existentes

--- admin/src/categories/categories_display.c
+++ admin/src/categories/categories_display.c
@@ -7,26 +7,26 @@ void categories_display() {
 
     clear();
 
-    mvprintw(4, 1, "1. Listar Categorias");
-    mvprintw(5, 1, "2. Atualizar Categorias");
-    mvprintw(6, 1, "3. Criar Categorias");
-    mvprintw(7, 1, "4. Voltar");
+    mvprintw(1, 1, "Lista de categorias");
+    mvprintw(3, 1, "Frutas pereciveis");
+    mvprintw(4, 1, "Frutas citricas");
+    mvprintw(5, 1, "Frutas tropicais");
+    mvprintw(6, 1, "Frutas de caroço");
+    mvprintw(8, 1, "1. Voltar");
     attroff(COLOR_PAIR(1) | A_BOLD);
     refresh();
     char option = getch();
 
      switch(option) {
     case '1':
-        product_display();
-        break;
-    case '2':
-        supplier_display();
-        break;
-        case '3':
-            categories_display();
-        break;
-    case '4':
         administrator_display();
+        break;
+    default:
+        clear();
+        mvprintw(1, 1, "Insira uma opcao valida");
+        refresh();
+        napms(100);
+        categories_display();
         break;
     }
 }
